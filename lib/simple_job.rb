7b71bf16b469e076e3f7a5a9974939508d9370d7
David Dawson <daws23@gmail.com>
2014-06-20 11:09:59 -0700

    adding TestJobQueue implementation

--- lib/simple_job.rb
+++ lib/simple_job.rb
@@ -2,6 +2,8 @@ module SimpleJob
 
   autoload :JobDefinition, 'simple_job/job_definition'
   autoload :JobQueue, 'simple_job/job_queue'
-  autoload :SQSJobQueue, 'simple_job/sqs_job_queue'
 
 end
+
+require 'simple_job/test_job_queue'
+require 'simple_job/sqs_job_queue'
